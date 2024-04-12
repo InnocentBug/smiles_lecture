bf783341361d1a2cadd615ac98b19d7bb5e94ded
Ludwig Schneider <ludwigschneider@uchicago.edu>
2024-04-12 12:23:03 -0500

    fixes

--- util.py
+++ util.py
@@ -1,15 +1,10 @@
-import numpy as np
-from rdkit import Chem
-from rdkit.Chem.Draw import IPythonConsole
-from IPython.display import SVG
+from urllib.parse import quote
 
+import requests
+from IPython.display import SVG
+from rdkit import Chem
 from rdkit.Chem import rdDepictor
 from rdkit.Chem.Draw import rdMolDraw2D
-import requests
-
-import bigsmiles_gen
-from bigsmiles_gen import System, mol_prob, Molecule, System
-from urllib.parse import quote
 
 
 def render_svg(svg):
@@ -28,8 +23,9 @@ def moltosvg(mol, molSize=(450, 150), kekulize=False):
     if kekulize:
         try:
             Chem.Kekulize(mc)
-        except:
+        except Exception as exc:
             mc = Chem.Mol(mol.ToBinary())
+            print(exc)
     if not mc.GetNumConformers():
         rdDepictor.Compute2DCoords(mc)
     drawer = rdMolDraw2D.MolDraw2DSVG(molSize[0], molSize[1])
@@ -42,7 +38,8 @@ def moltosvg(mol, molSize=(450, 150), kekulize=False):
 def render_mol(mol, **kwargs):
     return render_svg(moltosvg(mol, **kwargs))
 
+
 def get_iupac_name(smiles):
-    url = f"https://cactus.nci.nih.gov/chemical/structure/{smiles}/iupac_name"
-    response = requests.get(url)
+    url = f"https://cactus.nci.nih.gov/chemical/structure/{quote(smiles)}/iupac_name"
+    response = requests.get(url, timeout=1)
     return response.text
